b7443fdca9c1446f2ce87006dd1825d0228cc3d1
trabbAC4 <tr1476@nyu.edu>
2024-02-12 17:32:19 -0500

    Calendly set up

--- app/BookNow/page.tsx
+++ app/BookNow/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 import React, { useEffect } from "react";
+import Booking from '../components/Calender'
 
 export default function BookNow() {
     return( 
-
+        <div>
+            <h1>Schedule an Appointment</h1>
+            <Booking url="https://calendly.com/waypointtechnologies/30min" />
+      </div>
     )
 } 
