1048c7529f0d852c90323387e0d94d1ddc791bef
Tahmidur_Rabb <tr1476@nyu.edu>
2024-02-14 16:24:23 -0500

    Quick fix

--- app/BookNow/page.tsx
+++ app/BookNow/page.tsx
@@ -1,12 +1,11 @@
 'use client'
 import React, { useEffect } from "react";
-import Booking from '../components/Calender'
+
 
 export default function BookNow() {
     return( 
         <div>
             <h1>Schedule an Appointment</h1>
-            <Booking url="https://calendly.com/waypointtechnologies/30min" />
       </div>
     )
 } 
--- app/components/Calender.tsx
+++ app/components/Calender.tsx
@@ -1,24 +1,32 @@
 import React, { useEffect } from "react";
 
-const Calender = ({ url }) => {
-  
-  useEffect(() => {
-    const head = document.querySelector("head");
-    const script = document.createElement("script");
-    script.setAttribute(
-      "src",
-      "https://assets.calendly.com/assets/external/widget.js"
-    );
-    head.appendChild(script);
-  }, []);
 
-  return (
-    <div
-      className="calendly-inline-widget"
-      data-url={url}
-      style={{ minHeight: "800px", width: "100%" }}
-    ></div>
-  );
-};
+export default function Calender() {
+  return(
+    <h1> Simple info </h1> 
 
-export default Calender;
+  )
+}
+
+// const Calender = ({ url }) => {
+
+//   useEffect(() => {
+//     const head = document.querySelector("head");
+//     const script = document.createElement("script");
+//     script.setAttribute(
+//       "src",
+//       "https://assets.calendly.com/assets/external/widget.js"
+//     );
+//     head.appendChild(script);
+//   }, []);
+
+//   return (
+//     <div
+//       className="calendly-inline-widget"
+//       data-url={url}
+//       style={{ minHeight: "800px", width: "100%" }}
+//     ></div>
+//   );
+// };
+
+// export default Calender;
