862f7fffa0b841763d022c857afb5f209b019cf2
trabbAC4 <tr1476@nyu.edu>
2024-02-19 16:48:57 -0500

    Extra stages

--- app/BookNow/page.tsx
+++ app/BookNow/page.tsx
@@ -7,7 +7,7 @@ export default function BookNow() {
     return( 
         <div className="container">
       <h1>Schedule an Appointment</h1>
-            <Calender url="YOUR_CALENDLY_EVENT_LINK_HERE" />
+            <Calender url="https://calendly.com/waypointtechnologies" />
             </div>
     )
 } 
--- app/components/Calender.tsx
+++ app/components/Calender.tsx
@@ -1,32 +1,28 @@
 import React, { useEffect } from "react";
 
 
-export default function Calender() {
-  return(
-    <h1> Simple info </h1> 
 
-  )
-}
+const Calender = ({ url }) => {
 
-// const Calender = ({ url }) => {
+  useEffect(() => {
+    const head = document.querySelector("head");
+    const script = document.createElement("script");
+    script.setAttribute(
+      "src",
+      "https://assets.calendly.com/assets/external/widget.js"
+    );
+    head.appendChild(script);
+  }, []);
 
-//   useEffect(() => {
-//     const head = document.querySelector("head");
-//     const script = document.createElement("script");
-//     script.setAttribute(
-//       "src",
-//       "https://assets.calendly.com/assets/external/widget.js"
-//     );
-//     head.appendChild(script);
-//   }, []);
+  return (
+    <div
+      className="calendly-inline-widget"
+      data-url={url}
+      style={{ minHeight: "800px", width: "100%" }}
+    ></div>
+  );
+};
 
-//   return (
-//     <div
-//       className="calendly-inline-widget"
-//       data-url={url}
-//       style={{ minHeight: "800px", width: "100%" }}
-//     ></div>
-//   );
-// };
 
-// export default Calender;
+export default Calender; 
+
