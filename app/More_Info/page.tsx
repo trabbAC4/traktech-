64a1378f479f2c68e6c23abe97833171d629b44e
Tahmidur_Rabb <tr1476@nyu.edu>
2024-02-22 01:29:11 -0500

    Flexbox necesary

--- app/More_Info/page.tsx
+++ app/More_Info/page.tsx
@@ -2,6 +2,7 @@
 import Footer from '../components/Footer'
 import Navbar from '../components/Nav'
 import Survey from '../components/Survey'
+import Image from 'next/image'
 
 
 
@@ -10,13 +11,21 @@ import Survey from '../components/Survey'
 
 export default function More_Info() {
     return (
-        <main>
-           <div className="flex flex-col items-center justify-center h-screen">
-             <h2 className="text-3xl font-bold border-b-2 border-gray-300 mb-4 pb-2">
-                Please place your responses to the following questions so that we can assist you further
-             </h2>
-                <Survey />
+        <>
+         <div className = "gap-8 columns-2">
+            <div className = "col-span-1"> Testing </div>
+
+            <div className = "col-span-4 w-full h-full"> 
+                <Image 
+                    src= "/trucking.jpg"
+                    height=  {300}
+                    width = {300}
+                    alt = "truck picture"
+                /> 
+
+            
             </div>
-        </main>
+         </div>
+        </> 
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -246,13 +246,28 @@ export default function Home() {
       </div>
 
     </section>
+    <section> 
+      <h1 className=  "text-3xl md: text-4xl lg:text-5xl font-bold mb-8 text-center p-4" > Contact us for a free consultation </h1> 
+      <h2 className = "text-xl md: text-xl lg:text-xl font-bold mb-8 text-center p-4"> We would love to speak with you and for you to understand our product </h2>
+
+      <Image 
+        src="/meeting.jpg"
+        className="object-contain justify-center h-full w-full"
+        width={300}  // Adjust the width to make the image smaller
+        height={150} // Adjust the height to maintain the aspect ratio
+        alt="random image"
+      />
+
+      
+    </section>
 
 
 
     
     
-      <section className="w-full h-screen flex flex-col items-center justify-center py-4 md:py-24 lg:py-32">
-        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-8">Connect with us</h2>
+      <section className="w-full h-screen flex flex-col items-center justify-center">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-8">Revolutionizing tracking technology with mobile</h2>
+
         <div className="w-full flex justify-center">
     </div>
     </section>
@@ -265,27 +280,6 @@ export default function Home() {
 
 
     <section> 
-
-    <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-  <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
-  </div>
-  <div className="p-6">
-    <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-     Tailwind card
-    </h5>
-    <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula. 
-    </p>
-  </div>
-  <div className="p-6 pt-0">
-    <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-      Read More
-    </button>
-  </div>
-</div>
-
-
-  
   </section> 
 
 
