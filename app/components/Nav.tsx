cfa32fe04120a1439e0c84372bbceb4f447f6cb7
trabbAC4 <tr1476@nyu.edu>
2024-02-28 15:23:20 -0500

    Found a nice button

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -39,9 +39,13 @@ export default function Nav() {
 
       {/* Free Demo Button and Dropdown */}
       <div className="flex lg:flex items-center">
-        <Link href= "/More_Info">
-        <button className="bg-green-500 text-white px-4 py-2 rounded-md hidden lg:block">Free Demo</button>
-        </Link>
+      <Link href="/More_Info" className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-black-600 border-2 border-black-600 rounded-full hover:text-white group hover:bg-gray-50">
+        <span className="absolute left-0 block w-full h-0 transition-all bg-green-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+        <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+          <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+            </span>
+                <span className="relative">Free Demo</span>
+      </Link>
         {/* Dropdown */}
         <div className="relative lg:hidden">
           <button className="text-black px-4 py-2" onClick={() => setShowDropdown(!showDropdown)}>
@@ -50,16 +54,14 @@ export default function Nav() {
             </svg>
           </button>
           {showDropdown && (
-            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg">
-              <Link href= "/About" className="block px-4 py-2 text-black hover:bg-gray-100">About</Link>
-              <Link href="/Contact" className="block px-4 py-2 text-black hover:bg-gray-100">Contact</Link>
-              <Link href="/Technology" className="block px-4 py-2 text-black hover:bg-gray-100">Technology</Link>
+            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+            <Link href= "/About" className="block px-4 py-2 text-black hover:bg-gray-100">About</Link>
+            <Link href="/Contact" className="block px-4 py-2 text-black hover:bg-gray-100">Contact</Link>
+            <Link href="/Technology" className="block px-4 py-2 text-black hover:bg-gray-100">Technology</Link>
             </div>
+          
           )}
         </div>
-        <Link href= "/More_Info"> 
-        <button className="bg-green-500 text-white px-4 py-2 rounded-md lg:hidden">Free Demo</button>
-        </Link> 
         
       </div>
     </nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'emeraldgreen': '#022c22',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+
   },
   plugins: [],
 }
