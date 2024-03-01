f351d10f18758220d54523e6cc2829111fdc4b5d
Tahmidur_Rabb <tr1476@nyu.edu>
2024-03-01 12:51:53 -0500

    Train work

--- app/Technology/Products/page.tsx
+++ app/Technology/Products/page.tsx
@@ -54,34 +54,109 @@ export default function Product() {
         </div>
 
         </section>
-
-                 <section> 
-                    <h1> GPS Tracking </h1>
-
-                    
-                 </section>
-
-            <section>
-                <h1> ELD Logging </h1>
-            </section>
-
-            <section> 
-
-         <div className="flex flex-wrap justify-center lg:justify-between max-w-xl mx-auto">
-      <div className="border p-4 lg:w-1/3 w-full mb-4 lg:mb-0">
-        <h2 className="text-xl font-bold mb-2 text-center">Title 1</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <section className="bg-gray-2 dark:bg-dark pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+   <div className="container mx-auto">
+      <div className="flex flex-wrap -mx-4">
+         <div className="w-full px-4 md:w-1/2 xl:w-1/3">
+            <div
+               className="mb-10 overflow-hidden duration-300 bg-white rounded-lg dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3"
+               >
+               <img
+                  src="https://cdn.tailgrids.com/2.0/image/application/images/cards/card-01/image-01.jpg"
+                  alt="image"
+                  className="w-full"
+                  />
+               <div className="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        className="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
+                        >
+                     50+ Best creative website themes & templates
+                     </a>
+                  </h3>
+                  <p
+                     className="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7"
+                     >
+                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                     elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                     href="javascript:void(0)"
+                     className="inline-block py-2 text-base font-medium transition border rounded-full text-body-color hover:border-primary hover:bg-primary border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
+                     >
+                  View Details
+                  </a>
+               </div>
+            </div>
+         </div>
+         <div className="w-full px-4 md:w-1/2 xl:w-1/3">
+            <div
+               className="mb-10 overflow-hidden duration-300 bg-white rounded-lg dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3"
+               >
+               <img
+                  src="https://cdn.tailgrids.com/2.0/image/application/images/cards/card-01/image-02.jpg"
+                  alt="image"
+                  className="w-full"
+                  />
+               <div className="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        className="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
+                        >
+                     The ultimate UX and UI guide to card design
+                     </a>
+                  </h3>
+                  <p className="text-base leading-relaxed text-body-color mb-7">
+                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                     elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                     href="javascript:void(0)"
+                     className="inline-block py-2 text-base font-medium transition border rounded-full text-body-color hover:border-primary hover:bg-primary border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
+                     >
+                  View Details
+                  </a>
+               </div>
+            </div>
+         </div>
+         <div className="w-full px-4 md:w-1/2 xl:w-1/3">
+            <div
+               className="mb-10 overflow-hidden duration-300 bg-white rounded-lg dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3"
+               >
+               <img
+                  src="https://cdn.tailgrids.com/2.0/image/application/images/cards/card-01/image-03.jpg"
+                  alt="image"
+                  className="w-full"
+                  />
+               <div className="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        className="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
+                        >
+                     Creative Card Component designs graphic elements
+                     </a>
+                  </h3>
+                  <p className="text-base leading-relaxed text-body-color mb-7">
+                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                     elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                     href="javascript:void(0)"
+                     className="inline-block py-2 text-base font-medium transition border rounded-full text-body-color hover:border-primary hover:bg-primary border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
+                     >
+                  View Details
+                  </a>
+               </div>
+            </div>
+         </div>
       </div>
-      <div className="border p-4 lg:w-1/3 w-full mb-4 lg:mb-0">
-        <h2 className="text-xl font-bold mb-2 text-center">Title 2</h2>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div>
-      <div className="border p-4 lg:w-1/3 w-full">
-        <h2 className="text-xl font-bold mb-2 text-center">Title 3</h2>
-        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-      </div>
-    </div>
-    </section> 
+   </div>
+</section>
+          
+          
         
         </>
     )
