6e66b7946b0a6bf261eeaef530b17a3fee3a8f97
Tahmidur_Rabb <tr1476@nyu.edu>
2024-03-04 00:46:17 -0500

    Final stages before email incorporation

--- app/api/emailing/route.ts
+++ app/api/emailing/route.ts
@@ -1,9 +1,1 @@
 import nodemailer from 'nodemailer'
-
-
-export async function POST(request) {
-    const {subject, message} = await request.json();
-
-    const transporter = nodemailer.createTransport({
-    })
-}
