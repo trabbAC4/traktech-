0c5d73205ec0bdca01521324e6750b41a2707dd4
tasstix <152426679+tasstix@users.noreply.github.com>
2024-03-12 15:20:33 -0400

    added meta data to head to change tab appearance

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ import { LiaTruckSolid } from "react-icons/lia";
 import { HiMiniDevicePhoneMobile } from "react-icons/hi2";
 import { TbGps } from "react-icons/tb";
 import { FaUserCheck } from "react-icons/fa6";
-
+import Head from 'next/head';
 
 
 
@@ -51,6 +51,13 @@ export default function Home() {
 
   return(
   <>  
+  
+      <Head>
+        <title>WayPoint Technologies</title>
+        <meta name="description" content="Unlock the future of tracking technology with our precise ELD Logging and efficient fleet management services." />
+        
+      </Head>
+
 
 <div className="relative"> {/* This div is used to position the video and text relative to each other */}
   <video 
