cbfa10e9ed04b6a1f2fd43d65d16769e1817aed9
tasstix <152426679+tasstix@users.noreply.github.com>
2024-03-12 15:10:49 -0400

    contact form center update

--- app/More_Info/page.tsx
+++ app/More_Info/page.tsx
@@ -19,9 +19,10 @@ export default function More_Info() {
   <div className="flex flex-col lg:flex-row">
   {/* Left side */}
   <div className="lg:w-1/2 bg-gray-200 p-8">
-    <h2 className="text-2xl font-bold mb-4">Contact Us for a FREE demo</h2>
-    <h3 className= "text-xl mb-4"> Take the first step in expanding your business with our technology </h3>
+    <h2 className="text-2xl font-bold mb-4 flex justify-center items-center">Contact Us for a FREE demo</h2>
+    <h3 className= "text-xl mb-4 flex justify-center items-center"> Take the first step in expanding your business with our technology </h3>
     
+    <div className="flex justify-center items-center ">   
     <form className="w-full max-w-md">
       <div className="mb-4 w-full">
         <input
@@ -77,18 +78,19 @@ export default function More_Info() {
           className="border border-gray-400 rounded-md px-4 py-2 w-1/2 focus:outline-none focus:border-blue-500 transition-all duration-300"
         />
       </div>
-      
+      <div className="flex justify-center items-center "> 
       <Link href="/BookNow" className="shadow-2xl relative inline-flex items-center justify-center px-16 py-3 overflow-hidden text-lg font-medium text-black border-2 border-black rounded-full hover:text-white group hover:bg-emerald-green-950">
         <span className="absolute left-0 block w-full h-0 transition-all bg-purp opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
         <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
           <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
           </span>
-          <span className="relative">Request a Demo</span>
+          <span className="relative ">Request a Demo</span>
       </Link>
-
+</div>
 
 
     </form>
+    </div>
   </div>
   {/* Right side */}
   <div className="lg:w-1/2 relative"> 
