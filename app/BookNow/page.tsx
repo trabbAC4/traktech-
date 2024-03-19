9e59ad04edca0c341959bc2bf912e97972010a48
tasstix <152426679+tasstix@users.noreply.github.com>
2024-03-19 14:56:49 -0400

    Changed booking from calendly to google calender

--- app/BookNow/page.tsx
+++ app/BookNow/page.tsx
@@ -7,7 +7,11 @@ export default function BookNow() {
     return( 
         <>
         <div>
-        <InlineWidget url = "https://calendly.com/waypointtechnologies" /> 
+
+<iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ29LwnRvYRXI8ENGb3jQUZfHMuIct2iClx8PpRoMu_sDOZy1jyO168Kq2rv9S_go328uWeV5WIP?gv=true" 
+
+style ={{border: "0" }} width="100%" height="600" ></iframe>
+
         </div>
         </>  
     )
